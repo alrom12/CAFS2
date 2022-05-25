b7b176ba02a81cd36fd38598f6fba123f8b78946
Aleksandr <dev@alrom.de>
2022-05-25 19:26:44 +0300

    js

--- 2022-05-25-JS/a2/app.js
+++ 2022-05-25-JS/a2/app.js
@@ -4,14 +4,17 @@ for (let i = 0; i < 5; i++) {
     console.log(a[i]);
     }
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 console.log(Math.max(...a));
 console.log(Math.min(...a));
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_number_between_two_values
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
 
-function financial(x) {
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+  function financial(x) {
     return Number.parseFloat(x).toFixed(2);
   }
 
