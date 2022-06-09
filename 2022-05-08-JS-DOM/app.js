f975515794bcb4cb7f36c33c0cab5d6d7bc0ab70
Aleksandr <dev@alrom.de>
2022-06-09 18:54:49 +0300

    js-dom

--- 2022-05-08-JS-DOM/app.js
+++ 2022-05-08-JS-DOM/app.js
@@ -6,3 +6,16 @@
 
 
 console.log(document.getElementById('age-table').childNodes);
+console.log('getElementById = age-table');
+console.log(document.getElementById('age-table'));
+console.log('querySelector');
+console.log(document.querySelector('#age-table'));
+
+console.log('getElementsByName = age');
+console.log(document.getElementsByName('age'));
+console.log('querySelectorAll All');
+console.log(document.querySelectorAll('#age'));
+
+let labels = document.getElementsByTagName('label');
+console.log('getElementsByTagName = label');
+console.log(labels);
