31c85947774fae2b7b78bb38ee960e13c639dab9
Aleksandr <dev@alrom.de>
2022-06-13 19:33:11 +0300

    js-dom

--- 2022-06-09-JS-DOM/app.js
+++ 2022-06-09-JS-DOM/app.js
@@ -1,12 +1,15 @@
 const btn = document.querySelector('button');
-const write = document.querySelector('input:first-child');
-const read = document.getElementById("read");
+btn?.addEventListener('click', getValueFromInput)
 
-if (btn) {
-    btn?.addEventListener('click', getValueFromInput)
-}
+// if (btn) {
+//     btn.addEventListener('click', getValueFromInput)
+// }
 
 function getValueFromInput (event) {
-    console.log(write.value);
-    read?.value = write?.value;
+    const write = document.querySelector('input:first-child');
+    const read = document.getElementById("read");
+    console.log(write?.value);
+    if ((read) && (write)) {
+        read.value = write.value;
+    }
 }
