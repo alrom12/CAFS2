2eb11048301ba3731fd3deadb433a3aa3e1fc991
Aleksandr <dev@alrom.de>
2022-06-13 19:47:42 +0300

    js-dom-resize

--- 2022-06-13-JS-DOM/app.js
+++ 2022-06-13-JS-DOM/app.js
@@ -1,6 +1,8 @@
 window.addEventListener('resize', function(event){
 
     let widthHeight = [window.innerWidth, window.innerHeight];
-    document.querySelector('h1').textContent = `${widthHeight[0]} x ${widthHeight[1]}`;
+    if (document.querySelector('h1')) {
+        document.querySelector('h1').textContent = `${widthHeight[0]} x ${widthHeight[1]}`;
+    }
 
 });
