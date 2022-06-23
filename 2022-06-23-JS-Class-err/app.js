45c3b08942bec10f886669282e37fa5ce6ca576d
Aleksandr <dev@alrom.de>
2022-06-23 20:44:43 +0300

    JS-Class

--- 2022-06-23-JS-Class-err/app.js
+++ 2022-06-23-JS-Class-err/app.js
@@ -4,7 +4,10 @@ class Animal {
     }
 }
 class Rabbit extends Animal {
-    created = Date.now();
+    constructor(name) {
+        super(name);
+        this.created = Date.now();
+    }
 }
-let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+let rabbit = new Rabbit("White Rabbit");
 console.log(rabbit);
