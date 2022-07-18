75d36e686d5243eb2463ee06ec25c89f7174b97c
alrom12 <dev@alrom.de>
2022-07-18 12:28:57 +0300

    wip node server

--- 2022-07-15-nodejs-hw/server/server.js
+++ 2022-07-15-nodejs-hw/server/server.js
@@ -1,43 +1,59 @@
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework
+
 const http = require('node:http');
 const fs = require('node:fs');
+const path = require('node:path');
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.json': 'application/json',
+    '.txt': 'text/plain',
+    '.xml': 'text/xml'
+};
+const PUBLIC = '../public/';
 
-function readServerFile(filePath, contentType, req, res) {
+http.createServer((request, response) => {
+    console.log('request: ', request.url);
+
+    let filePath = `.${request.url}`;
+    if (filePath === './') {
+        filePath = `${PUBLIC}index.html`;
+    } else if (filePath.includes('/api/v1/message')) {
+        console.log('kuku text data');
+        filePath = './sample.txt';
+    } else if (filePath.includes('/api/v1/user/')) {
+        console.log('kuku user data');
+        let arr = filePath.split('/');
+        console.log(arr);
+        filePath = './users.json';
+    } else if (filePath.includes('/api/v1/users')) {
+        console.log('kuku users data');
+        filePath = './users.json';
+    } else if (filePath.includes('/api/v1/posts')) {
+        console.log('kuku posts data');
+        filePath = './posts.json';
+    } else {
+        console.log('kuku 404');
+        filePath = `${PUBLIC}404.html`;
+    }
+
+    let contentType = mimeTypes[path.extname(filePath)];
+
     fs.readFile(filePath, (error, content) => {
         if (error) {
-            res.writeHead(404);
-            res.end(error.code);
-        } 
-        else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content);
+            if(error.code === 'ENOENT') {
+                fs.readFile('./404.html', (error, content) => {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content);
+                });
+            } else {
+                response.writeHead(500);
+                response.end(error.code);
+            }
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content);
         }
     });
-}
-
-http.createServer((req, res) => {
-    console.log(req.url);
-    if (req.url.includes('/api/v1/user/')) {
-        console.log('kuku user data');
-    }
-    else if (req.url === '/') {
-        console.log('index');
-        readServerFile('../static/index.html', 'text/html', req, res);
-    }
-    else if (req.url === '/api/v1/text') {
-        console.log('text');
-        readServerFile('./sample.txt', 'text/plain', req, res);
-    }
-    else if (req.url === '/api/v1/users') {
-        console.log('users');
-        readServerFile('./users.json', 'application/json', req, res);
-    }
-    else if (req.url === '/api/v1/posts') {
-        console.log('posts');
-        readServerFile('./posts.json', 'application/json', req, res);
-    }
-    else {
-        console.log('404');
-        readServerFile('../static/404.html', 'text/html', req, res);
-    }
-
 }).listen(3000);
